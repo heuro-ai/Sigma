31bd46b9a21a6e36f300dee953430bfc3563d9e6
Heuro <72121666+heuro-ai@users.noreply.github.com>
2025-09-05 14:03:00 +0530

    Fix Features page routing

--- components/Router.tsx
+++ components/Router.tsx
@@ -3,6 +3,7 @@ import { App } from '../App';
 import { AboutPage } from './pages/AboutPage';
 import { ComparePage } from './pages/ComparePage';
 import { FeaturesPage } from './pages/FeaturesPage';
+import { FeaturesPage } from './pages/FeaturesPage';
 import { PricingPage } from './pages/PricingPage';
 import { FAQPage } from './pages/FAQPage';
 import { PrivacyPolicyPage } from './pages/PrivacyPolicyPage';
@@ -39,6 +40,8 @@ export const Router: React.FC = () => {
         return <App />;
       case '/about':
         return <AboutPage />;
+      case '/features':
+        return <FeaturesPage />;
       case '/features':
         return <FeaturesPage />;
       case '/compare':
